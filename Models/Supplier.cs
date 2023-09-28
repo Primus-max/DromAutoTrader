b963ae4db1139763c0c8fce8c8e5414d22464d87
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-09-28 16:44:35 +0800

    Базовая модель предсьавления, базовый класс команды

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -3,10 +3,7 @@
     public class Supplier
     {
         public int Id { get; set; } // Уникальный идентификатор поставщика
-        public string Name { get; set; } // Название поставщика
-        public string Address { get; set; } // Адрес поставщика
-        public string ContactEmail { get; set; } // Контактный адрес электронной почты
-        public string PhoneNumber { get; set; } // Номер телефона поставщика
+        public string? Name { get; set; } = "Имя поставщика"; // Название поставщика       
     }
 
 }
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DromAutoTrader.ViewModels
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        private string? _title;
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            Title = "Is fucking worked";
+        }
+    }
+}
