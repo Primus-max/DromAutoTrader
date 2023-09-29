bc4d868aa3998316ab7afa9279bf5727ffd7d048
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-09-29 15:06:48 +0800

    Модель бренда, добавил в контекст

--- Data/Connection/AppContext.cs
+++ Data/Connection/AppContext.cs
@@ -6,6 +6,8 @@ namespace DromAutoTrader.Data.Connection
     internal class AppContext : DbContext
     {
         public DbSet<Supplier> Suppliers { get; set; } = null!;
+        public DbSet<Brand> Brands { get; set; } = null!;
+        public DbSet<Channel> Channels { get; set; } = null!;
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=main.db");
---
+++ Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using DromAutoTrader.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DromAutoTrader.Models
+{
+    class Brand:IBaseModel
+    {
+        public int Id { get; set; } // Уникальный идентификатор поставщика
+        public string? Name { get; set; }  // Название поставщика    
+    }
+}
