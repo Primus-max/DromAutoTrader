8929e5d1135d3adf319f09a1f9f66505d13d6fca
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-10-05 14:15:37 +0800

    Закончил визуализацию таблицы цен накруток, отображаю тестовые данные

--- Data/Connection/AppContext.cs
+++ Data/Connection/AppContext.cs
@@ -8,6 +8,7 @@ namespace DromAutoTrader.Data.Connection
         public DbSet<Supplier> Suppliers { get; set; } = null!;
         public DbSet<Brand> Brands { get; set; } = null!;
         public DbSet<Channel> Channels { get; set; } = null!;
+        public DbSet<TablePriceOfIncrease> TablePriceOfIncreases { get; set; } = null!;
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=main.db");
--- ViewModels/EditeChannelPageViewModel.cs
+++ ViewModels/EditeChannelPageViewModel.cs
@@ -1,52 +1,92 @@
 ﻿using DromAutoTrader.Models;
 using DromAutoTrader.Views;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using AppContext = DromAutoTrader.Data.Connection.AppContext;
 
 namespace DromAutoTrader.ViewModels
 {
-    internal class EditeChannelPageViewModel:BaseViewModel
+    internal class EditeChannelPageViewModel : BaseViewModel
     {
         #region Приватные поля
+        private TablePriceOfIncrease _priceIncrease = null!;
         private Channel _selectedChannel = null!;
+        private AppContext _db = null!;
+        private ObservableCollection<TablePriceOfIncrease> _tablePriceOfIncreases = null!;
         #endregion
 
         #region Публичные поля
+        public TablePriceOfIncrease PriceIncrease
+        {
+            get => _priceIncrease;
+            set => Set(ref _priceIncrease, value);
+        }
         public Channel SelectedChannel
         {
             get => _selectedChannel;
             set => Set(ref _selectedChannel, value);
         }
+        public ObservableCollection<TablePriceOfIncrease> TablePriceOfIncreases
+        {
+            get => _tablePriceOfIncreases;
+            set => Set(ref _tablePriceOfIncreases, value);
+        }
         #endregion
 
         #region Команды
-        public ICommand GetChannelCommand { get; } = null!;
+        public ICommand AddRowTablePriceOfIncreasesCommand { get; } = null!;
 
-        private bool CanGetChannelExecute(object p) => true;
+        private bool CanAddRowTablePriceOfIncreasesCommandExecute(object p) => true;
 
-        private void OnGetChannelExecuted(object sender)
+        private void OnAddRowTablePriceOfIncreasesCommandExecuted(object sender)
         {
-            Test();
+            AddRowTablePriceOfIncreases();
         }
+
+
         #endregion
+
         public EditeChannelPageViewModel()
         {
+            InitializeDatabase();
+
             // Получаю выбранный Channel в AllChannelPage через LocatorService
             SelectedChannel = LocatorService.Current.SelectedChannel;
 
+            #region Инициализация источников данных
+            TablePriceOfIncreases = new ObservableCollection<TablePriceOfIncrease>();
+            #endregion
+
             #region Инициализация команд
-            GetChannelCommand = new LambdaCommand(OnGetChannelExecuted, CanGetChannelExecute);
+            AddRowTablePriceOfIncreasesCommand = new LambdaCommand(OnAddRowTablePriceOfIncreasesCommandExecuted, CanAddRowTablePriceOfIncreasesCommandExecute);
             #endregion
         }
 
         #region Методы
-        public void Test()
+        private void AddRowTablePriceOfIncreases()
         {
-            string nameChannel = SelectedChannel.Name;
+            TablePriceOfIncreases.Add(new TablePriceOfIncrease() { ChannelId = 3345, From = 244, To = 3453, PriceIncrease = 234543 });
+        }
+
+        // Инициализирую базу данных
+        private void InitializeDatabase()
+        {
+            try
+            {
+                // Экземпляр базы данных
+                _db = new AppContext();
+                // гарантируем, что база данных создана
+                _db.Database.EnsureCreated();
+                // загружаем данные о поставщиках из БД
+                _db.TablePriceOfIncreases.Load();
+            }
+            catch (Exception)
+            {
+                // TODO сделать запись логов
+                //Console.WriteLine($"Не удалось инициализировать базу данных: {ex.Message}");
+            }
         }
         #endregion
     }
