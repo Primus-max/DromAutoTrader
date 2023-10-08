6d7fe533042ea30c41a5eb01cef370ce63e75baf
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-10-08 11:29:45 +0800

    Фильтрация брэндов

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using DromAutoTrader.Views;
 using DromAutoTrader.Views.Pages;
 using OfficeOpenXml;
 using OpenQA.Selenium;
+using System.Windows.Data;
 
 namespace DromAutoTrader
 {
@@ -63,6 +64,27 @@ namespace DromAutoTrader
             //    }
 
             //}
+        }
+
+        private void BrandCollection_Filter(object sender, System.Windows.Data.FilterEventArgs e)
+        {
+            if (e.Item is not Brand brand) return;
+            if (string.IsNullOrEmpty(brand.Name)) return;
+
+            string searhingText = FilterSearchParameterTextBox.Text;
+            if (string.IsNullOrEmpty(searhingText)) return;
+
+            if (brand.Name.Contains(searhingText, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void FilterSearchParameterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = (TextBox) sender;
+            var collection = (CollectionViewSource)textBox.FindResource("BrandsCollection");
+
+            collection.View.Refresh();
         }
 
 
