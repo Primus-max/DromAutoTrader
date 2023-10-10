dfb7d1ec4eaccf470c8bb5e2b0c312ca9f1b32c5
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-10-10 15:28:21 +0800

    Отображаю список брэндов для выбора

--- ViewModels/AddBrandsToChannelaWindowViewModel.cs
+++ ViewModels/AddBrandsToChannelaWindowViewModel.cs
@@ -1,11 +1,56 @@
-﻿namespace DromAutoTrader.ViewModels
+﻿using DromAutoTrader.Data;
+
+namespace DromAutoTrader.ViewModels
 {
-    public class AddBrandsToChannelaWindowViewModel
+    internal class AddBrandsToChannelaWindowViewModel : BaseViewModel
     {
+        #region Приватные свойства
+        private AppContext _db = null!;
+        private ObservableCollection<Brand> _brands = null!;
+        #endregion
+
+        #region Публичные свойства
+        public ObservableCollection<Brand> Brands
+        {
+            get => _brands;
+            set => Set(ref _brands, value); 
+        }
+        #endregion
+
+        #region Команды
+
+        #endregion
 
         public AddBrandsToChannelaWindowViewModel()
         {
-                
+
+            // Получаю базу данных
+            InitializeDatabase();
+
+            Brands = new ObservableCollection<Brand>(_db.Brands.ToList());
         }
+
+
+        #region Методы
+        // Метод получения базы данных
+        private void InitializeDatabase()
+        {
+            try
+            {
+                // Экземпляр базы данных
+                _db = AppContextFactory.GetInstance();
+                // загружаем данные о поставщиках из БД
+                _db.Brands.Load();
+            }
+            catch (Exception)
+            {
+                // TODO сделать запись логов
+                //Console.WriteLine($"Не удалось инициализировать базу данных: {ex.Message}");
+            }
+        }
+        #endregion
+
+
+
     }
 }
--- Views/Windows/AddBrandsToChannelaWindow.xaml.cs
+++ Views/Windows/AddBrandsToChannelaWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DromAutoTrader.Data;
+using DromAutoTrader.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,21 @@ namespace DromAutoTrader.Views.Windows
     /// </summary>
     public partial class AddBrandsToChannelaWindow : Window
     {
+       
         public AddBrandsToChannelaWindow()
         {
             InitializeComponent();
+
+            Loaded += Window_Loaded;
+
+        }
+
+       
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Brands = new ObservableCollection<Brand>(_db.Brands.ToList());
+            //BrandsListBox.ItemsSource = Brands;
+            //BrandsListBox.DisplayMemberPath = "Name";
         }
     }
 }
