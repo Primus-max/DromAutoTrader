554a3716908eee2b2746072aa43ec20368236772
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-10-30 06:21:49 +0800

    Перед миграцией

--- Data/Connection/AppContext.cs
+++ Data/Connection/AppContext.cs
@@ -13,6 +13,7 @@ namespace DromAutoTrader.Data.Connection
         public DbSet<PublishedPrice> PublishedPrices  { get; set; } = null!;
         public DbSet<ImageService> ImageServices { get; set; } = null!;
         public DbSet<BrandImageServiceMapping> BrandImageServiceMappings { get; set; } = null!;
+       // public DbSet<AdPublishingInfo> AdPublishingInfos { get; set; } = null!;
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=main.db");
