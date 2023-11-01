f6253bbba5bbf3189f180dc655912227930c85ca
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-11-01 10:45:44 +0800

    Модель BrandChannelMapping

---
+++ Models/BrandChannelMapping.cs
@@ -0,0 +1,12 @@
+﻿namespace DromAutoTrader.Models
+{
+    public class BrandChannelMapping
+    {
+        public int BrandId { get; set; }
+        public Brand? Brand { get; set; }
+
+        public int ChannelId { get; set; }
+        public Channel? Channel { get; set; }
+    }
+
+}
