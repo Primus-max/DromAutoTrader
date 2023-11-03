c4b5dc613502c07a621d8e90eaf9af0cf8c08e27
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-11-03 11:12:05 +0800

    Перед перенаправлением осного потока

---
+++ Models/PostingProgressItem.cs
@@ -0,0 +1,16 @@
+﻿namespace DromAutoTrader.Models
+{
+    public class PostingProgressItem
+    {
+        public string? ChannelName { get; set; }
+        public string? PriceName { get; set; } // Имя прайса
+        public string? ProcessName { get; set; } 
+        public int CurrentStage { get; set; }
+        public int TotalStages { get; set; }
+        public int MaxValue { get; set; }
+        public string? DatePublished { get; set; }
+        public string? GetFileButton { get; set; }
+        public string? PriceExportPath { get; set; } // Путь к экспортированному прайсу
+    }
+
+}
